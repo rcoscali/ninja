b99a0e1fca200b7efc28d5900e419717e83baf5a
Rémi Cohen-Scali <rcoscali@gmail.com>
2017-07-03 21:48:08 +0200

    Added -s & -S help messahes

--- src/ninja.cc
+++ src/ninja.cc
@@ -203,6 +203,8 @@ void Usage(const BuildConfig& config) {
 "  -C DIR   change to DIR before doing anything else\n"
 "  -f FILE  specify input build file [default=build.ninja]\n"
 "\n"
+"  -s N     run until N builds has been built\n"
+"  -S N     run until N % builds has been built\n"
 "  -j N     run N jobs in parallel [default=%d, derived from CPUs available]\n"
 "  -k N     keep going until N jobs fail [default=1]\n"
 "  -l N     do not start new jobs if the load average is greater than N\n"
